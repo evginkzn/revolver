5d5dee2d32b5e76d8182cef505a25a0ddaaa2e03
evginkzn <sokolov.kai5@yandex.ru>
2022-04-03 22:50:27 +0300

    Write first definition of home point finding

--- application/revolver.ino
+++ application/revolver.ino
@@ -7,6 +7,9 @@
 
 #define STEP_PER_TURNAROUND 400
 
+#define HOME_POINT_SENSOR_PIN 6
+#define CENTER_POINT_SENSOR_PIN 7
+
 typedef enum
 {
     REVOLVER_STATE_IDLE = 0,
@@ -24,11 +27,14 @@ static revolver_state_t revolver_state = REVOLVER_STATE_IDLE;
 
 void setup()
 {
+    pinMode(HOME_POINT_SENSOR_PIN, INPUT);
+    pinMode(CENTER_POINT_SENSOR_PIN, INPUT);
+
     step_motor_configure();
 
     #ifdef DEBUG
     Serial.begin(9600);
-    Serial.print("Initialization done\r\n");
+    Serial.println("Initialization done\n");
     #endif // ! DEBUG
 }
 
@@ -43,6 +49,10 @@ void step_motor_configure()
     step_motor.setAcceleration(400);
     step_motor.setTarget(800);
     step_motor.invertEn(true);
+
+    #ifdef DEBUG
+    Serial.println("Step motor configured");
+    #endif // ! DEBUG
 }
 
 void revolver_tick()
@@ -93,14 +103,40 @@ void revolver_tick()
     }
 }
 
-void revolver_calibrate()
+bool revolver_find_home()
 {
+    bool is_found_home = false;
+
+    #ifdef DEBUG
+    Serial.println("Start finding home point...");
+    #endif // ! DEBUG
+
     for(int i = 0; i < STEP_PER_TURNAROUND + STEP_PER_TURNAROUND / 2; ++i)
     {
         step_motor.step();
 
-        
+        if (digitalRead(HOME_POINT_SENSOR_PIN) == HIGH)
+        {
+            is_found_home = true;
+            step_motor.pos = 0;
+            break;
+        }
 
         delay(10);
     }
+
+    if (is_found_home)
+    {
+        #ifdef DEBUG
+        Serial.println("Home point found.");
+        #endif // ! DEBUG
+    }
+    else
+    {
+        #ifdef DEBUG
+        Serial.println("Home point not found! Error!");
+        #endif // ! DEBUG
+    }
+
+    return is_found_home;
 }
