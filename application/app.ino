201d22646fe8195d8dab74a34a8889e6dfdeef13
evginkzn <sokolov.kai5@yandex.ru>
2022-04-10 03:39:15 +0300

    Correct pins. Add pushed callback, and use it for next state in algorithm

--- application/app.ino
+++ application/app.ino
@@ -14,16 +14,16 @@
 
 #define STEP_PER_TURNAROUND 3200
 
-#define FIRST_TUBE_SENSOR_PIN 6
-#define TUBE_CENTER_SENSOR_PIN 7
+#define FIRST_TUBE_SENSOR_PIN 8
+#define TUBE_CENTER_SENSOR_PIN 9
 
 #define PUSHER_FIRST_SERVO_PIN 2
 #define PUSHER_SECOND_SERVO_PIN 3
 
-#define CAP_FIRST_SERVO_PIN 8
-#define CAP_SECOND_SERVO_PIN 9
+#define CAP_FIRST_SERVO_PIN 6
+#define CAP_SECOND_SERVO_PIN 7
 
-static const char version[] = "0.2";
+static const char version[] = "0.4";
 
 GStepper2<STEPPER2WIRE> step_motor(STEP_PER_TURNAROUND, PIN_PUL, PIN_DIR, PIN_ENA);
 
@@ -34,6 +34,8 @@ static Cap cap;
 
 static uint8_t tube_target_ = 0;
 static uint8_t current_tube_ = 0;
+
+static bool is_pushed_ = true;
 
 void setup()
 {
@@ -49,36 +51,50 @@ void setup()
 
     step_motor_configure();
 
-    revolver.init();
     pusher.init(PUSHER_FIRST_SERVO_PIN, PUSHER_SECOND_SERVO_PIN);
     cap.init(CAP_FIRST_SERVO_PIN, CAP_SECOND_SERVO_PIN);
+    revolver.init();
 
     FunctionSlot<uint8_t> selected_event_slot(tube_selected_event);
     revolver.attachOnSelectEvent(selected_event_slot);
+
+    FunctionSlot<bool> pushed_event_slot(pushed_event);
 
     #ifdef DEBUG
     Serial.println("Initialization done\n");
     #endif // ! DEBUG
 
 }
-//                                          90  330  Error   60  270   0  180  60  330  0
-uint8_t tubes_selecting_test_targets[10] = { 3,  11,    14,   2,   8,  0,   6,  2, 11,  0 };
+
+uint8_t tubes_selecting_test_targets[12] = { 1,  2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0 };
 unsigned long select_period_counter = 0;
 uint8_t selector = 0;
 
+bool is_calibrated = false;
+
 void loop()
 {
-    if (millis() - select_period_counter > 3000)
+    while ( ! is_calibrated)
     {
-        select_period_counter = millis();
-        if (revolver.state() == Revolver::StateIdle)
-        {
-            revolver.select_tube(tubes_selecting_test_targets[selector]);
+        is_calibrated = revolver.find_first_tube();
 
-            ++selector;
-            if (selector >= 10)
+    }
+
+    if (is_pushed_ && ! pusher.in_action())
+    {
+        if (millis() - select_period_counter > 5000)
+        {
+            select_period_counter = millis();
+            if (revolver.state() == Revolver::StateIdle)
             {
-                selector = 0;
+                is_pushed_ = false;
+                revolver.select_tube(tubes_selecting_test_targets[selector]);
+
+                ++selector;
+                if (selector >= 12)
+                {
+                    selector = 0;
+                }
             }
         }
     }
@@ -108,7 +124,15 @@ void tube_selected_event(uint8_t selected_tube)
     Serial.println(" selected");
     #endif // ! DEBUG
 
+    cap.open();
+
     pusher.make_push();
+}
+
+void pushed_event(bool)
+{
+    is_pushed_ = true;
+    cap.close();
 }
 
 void cap_open()
